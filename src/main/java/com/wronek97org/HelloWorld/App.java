6e18b43d83d218086d1d0ffb4385eccf553c9dd6
wronek97 <bianchidavide97@gmail.com>
2021-03-23 18:12:46 +0100

    Second Commit

---
+++ src/main/java/com/wronek97org/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.wronek97org.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
